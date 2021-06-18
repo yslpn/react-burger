97fe3ebb6094a43d5c2fda6a88ef00fcdd9462e2
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2021-06-18 19:51:49 +0300

    add props and propTypes

--- src/components/order-details/order-details.js
+++ src/components/order-details/order-details.js
@@ -1,12 +1,11 @@
-import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import styles from './order-details.module.css';
 import done from '../../images/done.svg';
 
 const OrderDetails = (props) => {
     return (
         <div className={styles.order}>
-            <p className={styles['order__id']}>034536</p>
+            <p className={styles['order__id']}>{props.data.order.number}</p>
             <p className={styles['order__id-text']}>идентификатор заказа</p>
             <img className={styles['order__done-img']} src={done} alt="Готово" width="120" height="120"/>
             <p className={styles['order__bottom-text']}>Ваш заказ начали готовить</p>
@@ -15,8 +14,8 @@ const OrderDetails = (props) => {
     );
 }
 
-// OrderDetails.propTypes = {
-//     data: PropTypes.object.isRequired
-// };
+OrderDetails.propTypes = {
+    data: PropTypes.object.isRequired
+};
 
 export default OrderDetails;
