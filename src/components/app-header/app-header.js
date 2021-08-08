2ff417f823c57d8e548e1a5eeaa0255f72f66a42
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2021-08-09 02:26:32 +0300

    new nav for main-header

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,37 +1,34 @@
-import React from 'react';
 import styles from './app-header.module.css';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 import mobileLogo from '../../images/logo.svg';
+import { NavLink } from 'react-router-dom';
 
 const AppHeader = () => {
-
     return (
         <header className={styles.header}>
             <nav className={`${styles['header__nav']} pt-4 pb-4`}>
-                <div className={`${styles['header__nav-item']} ${styles['header__nav-item--logo']}`}>
-                    <a href="##">
-                        <Logo />
-                        <img className={styles['header__logo-mobile']} src={mobileLogo} alt="React Burger Logo" />
-                    </a>
-                </div>
-                <div className={`${styles['header__nav-item']}`}>
+                <NavLink to="/" className={`${styles['header__nav-item']} ${styles['header__nav-item--logo']}`} activeClassName={`${styles['header__nav-item--active']}`} exact>
+                    <Logo />
+                    <img className={styles['header__logo-mobile']} src={mobileLogo} alt="React Burger Logo" />
+                </NavLink>
+                <NavLink to="/" className={`${styles['header__nav-item']}`} activeClassName={`${styles['header__nav-item--active']}`} exact>
                     <Button type="secondary" size="medium">
                         <BurgerIcon />
                         <span className="ml-2" >Конструктор</span>
                     </Button>
-                </div>
-                <div className={styles['header__nav-item']}>
+                </NavLink>
+                <NavLink to="/profile/orders" className={styles['header__nav-item']} activeClassName={`${styles['header__nav-item--active']}`}>
                     <Button type="secondary" size="medium">
                         <ListIcon />
                         <span className="ml-2">Лента&nbsp;заказов</span>
                     </Button>
-                </div>
-                <div className={styles['header__nav-item']}>
+                </NavLink>
+                <NavLink to="/profile" className={styles['header__nav-item']} activeClassName={`${styles['header__nav-item--active']}`}>
                     <Button type="secondary" size="medium">
                         <ProfileIcon />
                         <span className="ml-2">Личный&nbsp;кабинет</span>
                     </Button>
-                </div>
+                </NavLink>
             </nav>
         </header>
     );
