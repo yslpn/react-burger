ad723e3b4d3a03555769fbd03547365b6560d4cf
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2021-08-09 02:27:12 +0300

    profile page

--- src/pages/profile/profile.js
+++ src/pages/profile/profile.js
@@ -1,6 +1,19 @@
+import React from 'react';
+import styles from './profile.module.css';
+import { useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import UserProfile from 'components/user-profile/user-profile';
+
 const ProfilePage = () => {
+    const { userLogged } = useSelector(store => ({
+        userLogged: store.login.userLogged
+    }));
+
     return (
-        <p>ProfilePage</p>
+        !userLogged ? <Redirect to={'/login'} /> :
+            <section className={styles.wrapper}>
+                <UserProfile />
+            </section>
     );
 }
 
