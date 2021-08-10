367ac34f5d9a3c619ed12d38a658b26efb05bfab
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2021-08-10 21:35:27 +0300

    update ingredint

--- src/components/ingredient-details/ingredient-details.js
+++ src/components/ingredient-details/ingredient-details.js
@@ -1,32 +1,36 @@
 import styles from './ingredient-details.module.css';
 import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const IngredientDetails = () => {
-    const { ingredientDetails } = useSelector(store => ({
-        ingredientDetails: store.modal.ingredientDetails
+    const { ingredientDetails, ingredientsData } = useSelector(store => ({
+        ingredientDetails: store.modal.ingredientDetails,
+        ingredientsData: store.ingredients.ingredientsData
     }));
+    const { id } = useParams();
+    const currentIngredient = (id ? ingredientsData.find(item => item._id === id) : null) || ingredientDetails;
 
     return (
         <div className={styles.details}>
             <h3 className={styles['details__header']}>Детали ингредиента</h3>
-            <img src={ingredientDetails.image_large} alt={ingredientDetails.name} width="420" height="240" />
-            <p className={styles['details__name']}>{ingredientDetails.name}</p>
+            <img src={currentIngredient.image_large} alt={currentIngredient.name} width="420" height="240" />
+            <p className={styles['details__name']}>{currentIngredient.name}</p>
             <ul className={styles['details__list']}>
                 <li className={styles['details__list-item']}>
                     <p className={styles['details__item-text']}>Калории, ккал</p>
-                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{ingredientDetails.calories}</p>
+                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{currentIngredient.calories}</p>
                 </li>
                 <li className={styles['details__list-item']}>
                     <p className={styles['details__item-text']}>Белки, г</p>
-                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{ingredientDetails.proteins}</p>
+                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{currentIngredient.proteins}</p>
                 </li>
                 <li className={styles['details__list-item']}>
                     <p className={styles['details__item-text']}>Жиры, г</p>
-                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{ingredientDetails.fat}</p>
+                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{currentIngredient.fat}</p>
                 </li>
                 <li className={styles['details__list-item']}>
                     <p className={styles['details__item-text']}>Углеводы, г</p>
-                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{ingredientDetails.carbohydrates}</p>
+                    <p className={`${styles['details__item-text']} ${styles['details__item-text--data']}`}>{currentIngredient.carbohydrates}</p>
                 </li>
             </ul>
         </div>
