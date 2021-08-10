1bb70aeac7d26a841259b40fec109b52c835068a
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2021-08-10 21:34:15 +0300

    new actions

--- src/services/actions/login.js
+++ src/services/actions/login.js
@@ -1,5 +1,4 @@
-import { loginRequest, logoutRequest, registerRequest } from '../api';
-import { setCookie } from 'utils/cookie';
+import { loginRequest, logoutRequest, registerRequest, getUserRequest, updateUserRequest } from '../api';
 
 export const LOGIN_USER = 'LOGIN_USER';
 export const LOGOUT_USER = 'LOGOUT_USER';
@@ -7,8 +6,6 @@ export const LOGOUT_USER = 'LOGOUT_USER';
 export const login = (formData) => {
     return async function (dispatch) {
         const res = await loginRequest(formData);
-        console.log(res);
-        setCookie('token', res.accessToken);
         localStorage.setItem('token', res.refreshToken);
         localStorage.setItem('userName', res.user.name);
         localStorage.setItem('userEmail', res.user.email);
@@ -31,12 +28,43 @@ export const register = (formData) => {
 export const logout = () => {
     return async function (dispatch) {
         const res = await logoutRequest();
-        console.log(res);
-        setCookie('token', '');
-        localStorage.removeItem('token')
+        localStorage.removeItem('token');
         dispatch({
             type: LOGOUT_USER
         });
+        return res;
+    }
+}
+
+export const getUser = () => {
+    return async function (dispatch) {
+        const res = await getUserRequest();
+        if (res && res.success) {
+            localStorage.setItem('token', res.refreshToken);
+            localStorage.setItem('userName', res.user.name);
+            localStorage.setItem('userEmail', res.user.email);
+            dispatch({
+                type: LOGIN_USER,
+                name: res.user.name,
+                email: res.user.email
+            });
+            return res;
+        }
+    }
+}
+
+export const updateUser = (formData) => {
+    return async function (dispatch) {
+        const res = await updateUserRequest(formData);
+        if (res && res.success) {
+            localStorage.setItem('userName', res.user.name);
+            localStorage.setItem('userEmail', res.user.email);
+            dispatch({
+                type: LOGIN_USER,
+                name: res.user.name,
+                email: res.user.email
+            });
+        }
         return res;
     }
 }
