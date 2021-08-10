ac3fa93411728d6fe67768d546151e03b3c5cf48
Yaroslav Slepukhin <YarslavSlepukhin@gmail.com>
2021-08-10 21:33:41 +0300

    add ingredient page

---
+++ src/pages/igredient/igredient.js
@@ -0,0 +1,9 @@
+import IngredientDetails from "../../components/ingredient-details/ingredient-details"
+
+const IngredientPage = () => {
+    return (
+        <IngredientDetails />
+    );
+}
+
+export default IngredientPage;
---
+++ src/pages/igredient/index.js
@@ -0,0 +1,1 @@
+export { default as IngredientPage } from "./igredient";
